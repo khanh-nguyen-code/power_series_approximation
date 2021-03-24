7618125e722bd5c37d8b85995953bc858268af56
Nguyen Ngoc Khanh <nguyenngockhanh.pbc@gmail.com>
2021-03-25 00:03:20 +0800

    main.py

--- main.py
+++ main.py
@@ -8,13 +8,14 @@ s = InnerProdSubspace(domain=(-np.pi, +np.pi))
 f = np.sin
 
 for dim, sin_approx in enumerate(s.project(f, 0.001)):
-    if dim > 6:
+    if dim > 5:
         break
-    print(f"dim {dim}: {sin_approx}")
     x = np.arange(-np.pi, +np.pi, 0.1)
     y1 = f(x)
     y2 = sin_approx(x)
     plt.plot(x, y1, "r")
     plt.plot(x, y2, "b")
     plt.ylim(-2, +2)
+    plt.title(f"dim {dim}")
+    print(f"dim {dim}: {sin_approx}")
     plt.show()
